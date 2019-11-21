fafa181f5f9ff66b8339221a31a16c0284aab02e
Stefano Lentini <steflen98@gmail.com>
2019-11-21 19:20:26 +0100

    Specifiche implementate
    Manca decidere come gestire il tutto per quanto riguarda i tags

---
+++ JavaProject/src/RegistryRemotoTagServer.java
@@ -0,0 +1,7 @@
+import java.rmi.RemoteException;
+import java.rmi.registry.Registry;
+import java.rmi.registry.RegistryHandler;
+
+public interface RegistryRemotoTagServer extends RegistryRemotoTagClient, RegistryRemotoServer{
+	public void associaTag(String nomeLogico, String tag) throws RemoteException;
+}
